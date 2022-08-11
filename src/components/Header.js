84102de111848425134e2baef20403ff67604dcc
Giovanni Cristian Marrocco <marroccogc@gmail.com>
2022-08-11 13:05:24 +0200

    stile progetto

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import Typed from 'react-typed'
+import me from '../me.jpg'
 
 const Header = () => {
   return (
-<div className="jumbotron img-fluid">
+<div className="container-fluid header img-fluid text-center">
     <div className="container py-5 text-center">
-        <h1 className="display-3 font-weight-bold text-yellow">My Portfolio</h1>
-        <p className="my-3">Using simple jumbotron-style component, create a nice Bootstrap 4 header with a background image.</p>
-        <a href="/" role="button" className="my-5 btn btn-custom px-5">See All Features</a>
+        <h1 className="display-2 mb-2 font-weight-bold text-yellow">My Portfolio</h1>
+        <Typed 
+            className="typed-text display-6"
+            strings={["My Project", "My Goal", "My Attitude", "My Passion"]}
+            typeSpeed={40}
+            backSpeed={60}
+            loop
+        />
     </div>
+    
+    <div className="box my-5">
+        <div className="content">
+            <img className="img-fluid" src={me} alt="me" />
+        </div>
+    </div>
+
 </div>
   )
 }
