7c26a0597ed74f288bf0218ac0a747805337cf04
Giovanni Cristian Marrocco <marroccogc@gmail.com>
2022-08-12 17:26:38 +0200

    terminato esercizio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,66 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
-import Section from "./components/Section";
-import Hoverable from "./components/Hoverable";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {faCakeCandles} from "@fortawesome/free-solid-svg-icons";
+import {faMap} from "@fortawesome/free-solid-svg-icons";
+import {faPhone} from "@fortawesome/free-solid-svg-icons";
+import {faLock} from "@fortawesome/free-solid-svg-icons";
+import {useState} from 'react';
+
+const Section = () => {
+
+  const [label, setLabel] = useState("...");
+
+  const handleMouseOver = (event , lbl) => {
+    setLabel(lbl);
+  };
+
+  return (
+  <div>
+    <div className='text-center mt-5'>
+          <div>
+            <span className='label-custom'>
+                {label}
+            </span>
+          </div>
+      </div>
+
+
+      <div className="container icon-section">
+        <div className="row">
+          <div className="icon-list mt-5">
+              <FontAwesomeIcon onMouseEnter={event => handleMouseOver(event, 'Giovanni Cristian Marrocco')} className="icons fa-2x" icon={faUser}
+              
+              />
+              <FontAwesomeIcon onMouseEnter={event => handleMouseOver(event, 'marroccogc@gmail.com')} className="icons fa-2x" icon={faEnvelope} 
+              
+              />
+              <FontAwesomeIcon onMouseEnter={event => handleMouseOver(event, '17 Marzo 1995')} className="icons fa-2x" icon={faCakeCandles} 
+              
+              />
+              <FontAwesomeIcon onMouseEnter={event => handleMouseOver(event, 'Via Macerine 87 Cervaro')} className="icons fa-2x" icon={faMap} 
+              
+              />
+              <FontAwesomeIcon onMouseEnter={event => handleMouseOver(event, '+39 380 6422400')} className="icons fa-2x" icon={faPhone} 
+              
+              />
+              <FontAwesomeIcon onMouseEnter={event => handleMouseOver(event, 'scolapasta17')} className="icons fa-2x" icon={faLock} 
+              
+              />
+          </div>
+        </div>
+      </div>
+  </div>
+  )
+}
 
 function App() {
   return (
 < >
   <Header />
-  <Hoverable />
   <Section />
 </>
   )
--- src/components/Section.js
+++ src/components/Section.js
@@ -6,34 +6,46 @@ import {faCakeCandles} from "@fortawesome/free-solid-svg-icons"
 import {faMap} from "@fortawesome/free-solid-svg-icons"
 import {faPhone} from "@fortawesome/free-solid-svg-icons"
 import {faLock} from "@fortawesome/free-solid-svg-icons"
+import {useState} from 'react';
 
 const Section = () => {
+
+  const [isHovering , setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
     <div className="container icon-section">
       <div className="row">
         <div className="icon-list mt-5">
-            <FontAwesomeIcon className="icons fa-2x" icon={faUser}
+            <FontAwesomeIcon onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="icons fa-2x" icon={faUser}
             
             />
-            <FontAwesomeIcon className="icons fa-2x" icon={faEnvelope} 
+            <FontAwesomeIcon onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="icons fa-2x" icon={faEnvelope} 
             
             />
-            <FontAwesomeIcon className="icons fa-2x" icon={faCakeCandles} 
+            <FontAwesomeIcon onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="icons fa-2x" icon={faCakeCandles} 
             
             />
-            <FontAwesomeIcon className="icons fa-2x" icon={faMap} 
+            <FontAwesomeIcon onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="icons fa-2x" icon={faMap} 
             
             />
-            <FontAwesomeIcon className="icons fa-2x" icon={faPhone} 
+            <FontAwesomeIcon onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="icons fa-2x" icon={faPhone} 
             
             />
-            <FontAwesomeIcon className="icons fa-2x" icon={faLock} 
+            <FontAwesomeIcon onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="icons fa-2x" icon={faLock} 
             
             />
         </div>
       </div>
     </div>
-  )
+    )
 }
 
 export default Section
